cf7b6fb299652987331bd5bce2225283c5761d9a
Bruno <brunofsc_@hotmail.com>
2019-08-30 01:56:56 -0300

    terminar layout detalhes

---
+++ app/src/main/java/com/example/infinitybike/MenuNavigation_Activity.java
@@ -0,0 +1,14 @@
+package com.example.infinitybike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuNavigation_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_navigation_layout);
+    }
+}
---
+++ app/src/main/java/com/example/infinitybike/Status_Activity.java
@@ -0,0 +1,14 @@
+package com.example.infinitybike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Status_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.status_layout);
+    }
+}
